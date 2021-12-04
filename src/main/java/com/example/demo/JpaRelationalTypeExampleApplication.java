5237ebd0e9c5285883ff53454a9ec8608d6c9763
hairshkumar <cse2pasaga@gmail.com>
2021-12-04 13:17:08 +0530

    Created project file.

---
+++ src/main/java/com/example/demo/JpaRelationalTypeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRelationalTypeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRelationalTypeExampleApplication.class, args);
+	}
+
+}
